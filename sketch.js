83e737e1837a9288f159baa13926ed3de1a22fdc
Tony <anthony.valdovinos@sjsu.edu>
2019-10-08 00:38:22 -0700

    sound update

--- sketch.js
+++ sketch.js
@@ -1,42 +1,54 @@
-let array = [];
+let noiseOffset = 0.0;
+let strokeWidth = 5;
 
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(windowWidth, windowHeight);
   background(220, 550, 250);
 
-  strokeWeight(5);
-  noFill();
-}
 
+}
 
 function draw() {
+  background(220, 550, 250, 5);
+  strokeWeight(strokeWidth);
 
-  if (mouseIsPressed) {
-    // line(mouseX, mouseY, pmouseX, pmouseY);
-    background(0);
-    array.push([mouseX, mouseY]);
-  }
+
+  noiseOffset += 0.01;
+  strokeWidth = noise(noiseOffset) * 55;
+
+  stroke(map(mouseX, 0, 600, 0, 255, true))
+  line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+  line(mouseX, mouseY, pmouseX, pmouseY);
 
 }
+
+
 
 function keyTyped() {
 
   if (key === 's') {
     //save this image
     saveCanvas('filename', 'png');
-  } else if (key === 'd') {
+  } else if (key === 'c') {
+    clear();
     // display image
-    background(0);
-    background(220, 550, 250);
 
-beginShape();
-for(let i = 0; i < array.length; i++){
-  // line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
-curveVertex(array[i][0], array[i][1])
+
+    // // for my own reference vvvvv
+    // beginShape();
+    // for (let i = 0; i < array.length; i++) {
+    //   // line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
+    //   curveVertex(array[i][0], array[i][1])
+    // }
+    // endShape();
+
+  }
+
+  return false;
+
 }
-endShape();
-}
 
-return false;
-
+function mousePressed() {
+  array = [];
+  backgroundColor = 255;
 }
