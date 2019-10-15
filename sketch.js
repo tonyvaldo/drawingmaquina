5bedf3539e228416900017dff71dd45a6917140a
Tony <anthony.valdovinos@sjsu.edu>
2019-10-15 10:57:37 -0700

    update

--- sketch.js
+++ sketch.js
@@ -1,42 +1,54 @@
 let array = [];
+let wink;
+let stamp = [];
+let img;
+
+function preload() {
+
+img = loadImage('images/stamp1.png')
+}
 
 function setup() {
-  createCanvas(600, 600);
-  background(220, 550, 250);
+  let cnv = createCanvas(800, 600);
+  cnv.parent('container')
+noStroke();
+imageMode(CENTER);
 
-  strokeWeight(5);
   noFill();
 }
 
 
 function draw() {
 
-  if (mouseIsPressed) {
-    // line(mouseX, mouseY, pmouseX, pmouseY);
-    background(0);
-    array.push([mouseX, mouseY]);
+}
+
+
+function mousePressed(){
+  // for (let i = 1; i < stamp.length; i++){
+  //   stamp[i].clicked(mouseX, mouseY);
+
+  image(img, mouseX, mouseY, 50, 50);
   }
 
-}
 
-function keyTyped() {
 
-  if (key === 's') {
-    //save this image
-    saveCanvas('filename', 'png');
-  } else if (key === 'd') {
-    // display image
-    background(0);
-    background(220, 550, 250);
+// function keyTyped() {
 
-beginShape();
-for(let i = 0; i < array.length; i++){
-  // line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
-curveVertex(array[i][0], array[i][1])
-}
-endShape();
-}
+  // if (key === 's') {
+  //   //save this image
+  //   saveCanvas('filename', 'png');
+  // } else if (key === 'd') {
+  //   // display image
 
-return false;
 
-}
+// beginShape();
+// for(let i = 0; i < array.length; i++){
+//   line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
+// curveVertex(array[i][0], array[i][1])
+// }
+// endShape();
+// }
+//
+// // return false;
+// }
+// }
