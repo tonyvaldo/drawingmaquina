4ec4f4c7a618e92a96b55818210ef64fb4041817
Tony <anthony.valdovinos@sjsu.edu>
2019-10-24 16:41:59 -0700

    finally!

--- sketch.js
+++ sketch.js
@@ -1,24 +1,30 @@
 let array = [];
 let stamp = [];
-let img;
 let drawingbook;
+let currentStampImage;
+let eyeMarker;
+let winkMarker;
+let asteriskMarker;
+let exclamationMarker;
+let kissMarker;
+let spiralMarker;
+
 
 function preload() {
-
-  img = loadImage('images/stamp2.png')
   drawingbook = loadImage('images/drawingbook.png')
   stamp[1] = loadImage('images/stamp1.png');
-stamp[3] = loadImage('images/stamp3.png');
-stamp[4] = loadImage('images/stamp4.png');
-stamp[5] = loadImage('images/stamp5.png');
-stamp[6] = loadImage('images/stamp6.png');
-stamp[7] = loadImage('images/stamp7.png');
-stamp[8] = loadImage('images/stamp8.png');
-stamp[9] = loadImage('images/stamp9.png');
-stamp[10] = loadImage('images/stamp10.png');
-stamp[11] = loadImage('images/stamp11.png');
-stamp[12] = loadImage('images/stamp12.png');
-stamp[13] = loadImage('images/stamp13.png');
+  stamp[2] = loadImage('images/stamp2.png');
+  stamp[3] = loadImage('images/stamp3.png');
+  stamp[4] = loadImage('images/stamp4.png');
+  stamp[5] = loadImage('images/stamp5.png');
+  stamp[6] = loadImage('images/stamp6.png');
+  stamp[7] = loadImage('images/stamp7.png');
+  stamp[8] = loadImage('images/stamp8.png');
+  stamp[9] = loadImage('images/stamp9.png');
+  stamp[10] = loadImage('images/stamp10.png');
+  stamp[11] = loadImage('images/stamp11.png');
+  stamp[12] = loadImage('images/stamp12.png');
+  stamp[13] = loadImage('images/stamp13.png');
 
 }
 
@@ -29,8 +35,56 @@ function setup() {
   noStroke();
   imageMode(CENTER);
 
+  currentStampImage = stamp[2];
+
+  eyeMarker = select('#eyeMarker')
+  eyeMarker.mousePressed(changeStampToEye);
+
+  winkMarker = select('#winkMarker')
+  winkMarker.mousePressed(changeStampToWink);
+
+  asteriskMarker = select('#asteriskMarker')
+  asteriskMarker.mousePressed(changeStampToAsterisk);
+
+  exclamationMarker = select('#exclamationMarker')
+  exclamationMarker.mousePressed(changeStampToExclamation);
+
+  kissMarker = select('#kissMarker')
+  kissMarker.mousePressed(changeStampToKiss);
+
+  spiralMarker = select('#spiralMarker')
+  spiralMarker.mousePressed(changeStampToSpiral);
+
   noFill();
 }
+
+function changeStampToEye() {
+  currentStampImage = stamp[10];
+  console.log("clicked")
+}
+
+function changeStampToWink(){
+  currentStampImage = stamp[2];
+
+}
+function changeStampToAsterisk(){
+  currentStampImage = stamp[8];
+
+}
+function changeStampToExclamation(){
+  currentStampImage = stamp[11];
+
+}
+function changeStampToKiss(){
+  currentStampImage = stamp[9];
+
+}
+function changeStampToSpiral(){
+  currentStampImage = stamp[12];
+
+}
+
+
 
 
 function draw() {
@@ -42,7 +96,7 @@ function mousePressed() {
   // for (let i = 1; i < stamp.length; i++){
   //   stamp[i].clicked(mouseX, mouseY);
 
-  image(stamp[12], mouseX, mouseY, 25, 25);
+  image(currentStampImage, mouseX, mouseY, 25, 25);
 
 
 }
